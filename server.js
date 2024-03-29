57d6012a4445aa011d5dc318de7f21f66b6f3bb2
glauroqj <glauro.juliani@hotmail.com>
2024-03-29 18:42:21 -0300

    chore: change folder structure

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
-const Connection = require("./server/Connection");
-const HttpServer = require("./server/HttpServer");
-const LancamentoController = require("./server/LancamentoController");
-const LancamentoData = require("./server/LancamentoData");
-
+require("dotenv").config();
 const PORT = process.env.PORT;
+
+const Connection = require("./server/db/Connection");
+const HttpServer = require("./server/HttpServer");
+const LancamentoController = require("./server/controllers/lancamento/LancamentoController");
+const LancamentoData = require("./server/db/lancamento/LancamentoData");
 
 const connection = new Connection();
 const lancamentoData = new LancamentoData(connection);
+
 const httpServer = new HttpServer();
 new LancamentoController(httpServer, lancamentoData);
+
 httpServer.listen(PORT);
