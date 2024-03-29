f72d961d41a49b91b76f8e44e78c3db4e43298b7
glauroqj <glauro.juliani@hotmail.com>
2024-03-29 19:18:11 -0300

    chore: change folder structure

--- create.sql
+++ create.sql
@@ -3,7 +3,6 @@ create schema financas_pessoais;
 create table financas_pessoais.lancamento (
 	id_lancamento serial primary key,
 	mes text,
-	ano text,
 	categoria text,
 	tipo text,
 	valor numeric
